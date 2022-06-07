d5b34183a4c1dbeab25f4a635364ae377ac20c14
alex <AlexanderLorenzo@miamibeachfl.gov>
2022-06-07 14:44:52 -0400

    tied roll button to action that grabs random dice image and displays it

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    
+   
+    
+    @IBAction func rollButtonAction(_ sender: Any) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        firstDice.image = diceArray[Int.random(in: 0...5)]
+        secondDice.image = diceArray[Int.random(in: 0...5)]
+        
+    }
+    
 }
 
